2c1c02a250bc14cccde7bbef1ad0f4991d0078ba
Богдан <norestwithwest@Overlord.local>
2023-02-27 17:56:06 +0200

    fxd

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -9,12 +9,11 @@ const pool = new Pool({
 })
 
 
-client.pool('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+pool.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
     }
     pool.end();
   });
-
 module.exports = pool
