68138e9feaf692746943abddaea4fc3ddbc4e1a7
Богдан <norestwithwest@Overlord.local>
2023-02-27 17:54:29 +0200

    fxd

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -8,4 +8,13 @@ const pool = new Pool({
     database: process.env.PG_DB
 })
 
+
+client.pool('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    pool.end();
+  });
+
 module.exports = pool
