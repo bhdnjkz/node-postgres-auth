887273bceacf3ba5a72fa9ce50c0426214aff583
Богдан <norestwithwest@Overlord.local>
2023-02-27 14:16:27 +0200

    make it better

--- src/index.js
+++ src/index.js
@@ -113,13 +113,4 @@ let errors = []
     }
 })
 
-
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'world'], ' ');
-  return element;
-}
-
-document.body.appendChild(component());
-
 app.listen(PORT)
