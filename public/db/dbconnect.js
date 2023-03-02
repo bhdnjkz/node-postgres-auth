0169154b9b6ed06f44ab52e958f08f6d81373f36
Богдан <norestwithwest@Overlord.local>
2023-03-03 00:52:51 +0200

    cleanup initiated

--- public/db/dbconnect.js
+++ public/db/dbconnect.js
@@ -4,9 +4,11 @@ const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
     port: 5432,
-    password: "password",
+    password: "231445Doss",
     database: 'postgres'
 })
+//ssh password: aWaewa4reij;ie"
+
 
 pool.connect((err, client, done) => {
     if (err) throw err
@@ -20,8 +22,8 @@ pool.connect((err, client, done) => {
     })
     client.query(`SELECT COUNT (*) FROM countries`, (err, res) =>{
         if (err) throw err
-        if (res.rows[0].count === '0') {
-            client.query(`INSERT INTO "countries"("country_name") VALUES ('Ukraine'), ('USA'), ('Finland'), ('Canada')`)
+        if (res.rows.length === 0) {
+            client.query(`INSERT INTO "countries"("country_name") VALUES ("Ukraine"), ("USA"), ("Finland"), ("Canada")`)
         }
     })
     client.query(`CREATE TABLE IF NOT EXISTS public.users
@@ -36,8 +38,6 @@ pool.connect((err, client, done) => {
       done()
       if (err) {
         console.log(err.stack)
-    } else {
-        console.log(res.rows[0])
       }
     })
   })
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const pool = require('./db/dbconnect')
+const pool = require('./public/db/dbconnect')
 const app = express()
+const path = require('path')
 const session = require('express-session')
 const PORT = process.env.PORT || 30000
 
@@ -14,6 +15,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json()); 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+app.use(express.static((path.join(__dirname, 'public'))))
 
 
 //routes
